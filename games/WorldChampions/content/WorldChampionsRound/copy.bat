c8da5ee90a5950f8115891a0eea1d3da6dbb88e1
Eric <eric.boingo@gmail.com>
2022-07-23 00:30:02 +0200

    Champd Up Synchro

---
+++ games/WorldChampions/content/WorldChampionsRound/copy.bat
@@ -0,0 +1,2 @@
+@echo off
+for /D %%a in ("D:\Erizzle-Web\Nesjob\The-Jackbox-Party-Pack-7-German\games\WorldChampions\content\WorldChampionsRound\*.*") do xcopy /y /d "D:\Erizzle-Web\Nesjob\The-Jackbox-Party-Pack-7-German\games\WorldChampions\content\WorldChampionsRound\response.ogg" "%%a\"
