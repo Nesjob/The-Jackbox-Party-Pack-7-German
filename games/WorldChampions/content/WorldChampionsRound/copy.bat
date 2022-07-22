9aa7a9885620a02abef59129c9d02e536ce4eabf
Eric <eric.boingo@gmail.com>
2022-07-23 00:53:00 +0200

    Hotfixes

--- games/WorldChampions/content/WorldChampionsRound/copy.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-for /D %%a in ("D:\Erizzle-Web\Nesjob\The-Jackbox-Party-Pack-7-German\games\WorldChampions\content\WorldChampionsRound\*.*") do xcopy /y /d "D:\Erizzle-Web\Nesjob\The-Jackbox-Party-Pack-7-German\games\WorldChampions\content\WorldChampionsRound\response.ogg" "%%a\"
